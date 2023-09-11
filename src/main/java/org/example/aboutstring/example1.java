561007c71677a25848d75eaf9d35760fa29f13f2
buffpsi <rlawlstjd778@naver.com>
2023-09-11 13:40:36 +0900

    문자열 복습1_1

--- src/main/java/org/example/aboutstring/example1.java
+++ src/main/java/org/example/aboutstring/example1.java
@@ -8,13 +8,22 @@ public class example1 {
     public int solution(String str, char t) {
         int answer = 0;
 
-        //문자열의 길이를 나타낼때 .length() , 배열길이는 length로 표현가능
-        for (int i = 0; i < str.length(); i++) {
-            //문자열의 i번째 인덱스를 알아보는 라이브러리 함수 charAt(i)를 사용하면 쉽게 풀린다.
-            if (str.charAt(i) == t) {
+//        //해답1
+//        //문자열의 길이를 나타낼때 .length() , 배열길이는 length로 표현가능
+//        for (int i = 0; i < str.length(); i++) {
+//            //문자열의 i번째 인덱스를 알아보는 라이브러리 함수 charAt(i)를 사용하면 쉽게 풀린다.
+//            if (str.charAt(i) == t) {
+//                answer++;
+//            }
+//        }
+
+        //해당2 for each문을 사용 , 문자열을 문자배열로 만들어줘야함 코드 더 간결해짐
+        for (char a : str.toCharArray()){
+            if (a == t){
                 answer++;
             }
         }
+
         return answer;
     }
 
