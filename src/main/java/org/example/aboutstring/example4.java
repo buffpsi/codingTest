f91f80c494fa0738a92bcbcf507f93d29b8e4e79
buffpsi <rlawlstjd778@naver.com>
2023-09-13 13:54:21 +0900

    문자열 복습4_문자열 에서 중복 문자 찾을때 .indexof를 사용한 예제

--- src/main/java/org/example/aboutstring/example4.java
+++ src/main/java/org/example/aboutstring/example4.java
@@ -1,4 +1,25 @@
 package org.example.aboutstring;
 
+import java.util.Scanner;
+
+//문자열에서 중복 문자를 제거해야 할 때 => 해당 문자열에 있는 번쨰수와 indexof가 다르면된다
+//로직을 생각하자 컴퓨터적으로 초기아이디어는 한문자를 배열로넣고 한번씩 돌려가면서 있으면없애려고했다
 public class example4 {
+    public String solution(String str){
+        String answer = "";
+        for (int i=0; i<str.length(); i++){
+            if (str.indexOf(str.charAt(i)) == i){
+                answer += str.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        example4 e4 = new example4();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(e4.solution(str));
+    }
+
 }
