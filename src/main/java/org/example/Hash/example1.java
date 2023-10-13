1a172a698807bb22f9300cfa4827005d1592f599
buffpsi <rlawlstjd778@naver.com>
2023-10-13 23:36:51 +0900

    Hash기초 개념

---
+++ src/main/java/org/example/Hash/example1.java
@@ -0,0 +1,25 @@
+package org.example.Hash;
+
+import java.util.*;
+
+public class example1 {
+    public char solution(int n, String s) {
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        System.out.println(map.size());
+
+        int max = Integer.MIN_VALUE;
+        for (char key : map.keySet()) {
+//System.out.print]n(key+" "+map.get (key));
+            if (map.get(key) > max) {
+                max = map.get(key);
+                answer = key;
+            }
+        }
+        return answer;
+    }
+}
